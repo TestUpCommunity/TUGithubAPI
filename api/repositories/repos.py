8174d37246dd8c30211f422aff634f76b52f8f83
zhangting <89507288@qq.com>
2019-03-24 17:33:40 +0800

    format the code

--- api/repositories/repos.py
+++ api/repositories/repos.py
@@ -1,7 +1,9 @@
 from core.rest_client import RestClient
+
+
 class Repos(RestClient):
-    def __init__(self,api_root_url,**kwargs):
-        super(Repos, self).__init__(api_root_url,**kwargs)
+    def __init__(self, api_root_url, **kwargs):
+        super(Repos, self).__init__(api_root_url, **kwargs)
 
     def list_your_repos(self):
         return self.get("/user/repos")
--- github.py
+++ github.py
@@ -1,16 +1,17 @@
 from api.repositories.repos import Repos
 
+
 class Github():
     def __init__(self, **kwargs):
-        self.api_root_url="http://api.github.com"
-        self.repos = Repos(self.api_root_url,**kwargs)
+        self.api_root_url = "http://api.github.com"
+        self.repos = Repos(self.api_root_url, **kwargs)
 
 
 if __name__ == '__main__':
-    r=Github(token="xxxxxxxxxxxxxx")
-    x= r.repos.list_your_repos()
+    r = Github(token="xxxxxxxxxxxxxx")
+    x = r.repos.list_your_repos()
     print(x.text)
 
-    r=Github(username="xxxxxx",password="xxxxx")
-    x= r.repos.list_your_repos()
+    r = Github(username="xxxxxx", password="xxxxx")
+    x = r.repos.list_your_repos()
     print(x.text)
