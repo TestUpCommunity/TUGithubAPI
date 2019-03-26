3c98b9b9716548cda0c4957a31403ffda9e9ed94
xctester <269986229@qq.com>
2019-03-26 14:48:25 +0800

    new commit

--- core/rest_client.py
+++ core/rest_client.py
@@ -36,16 +36,16 @@ class RestClient():
         if method_name == "options":
             return self.session.options(url, **kwargs)
         if method_name == "head":
-            return self.session.head(self, url, **kwargs)
+            return self.session.head(url, **kwargs)
         if method_name == "put":
-            return self.session.put(self, url, data, **kwargs)
+            return self.session.put(url, data, **kwargs)
         if method_name == "patch":
-            return self.session.patch(self, url, data, **kwargs)
+            return self.session.patch(url, data, **kwargs)
         if method_name == "delete":
-            return self.session.delete(self, url, **kwargs)
+            return self.session.delete(url, **kwargs)
 
 
 if __name__ == '__main__':
     r = RestClient("http://httpbin.org")
     x = r.post("/post", json={"a": "b"})
-    print(x.text)
+    
