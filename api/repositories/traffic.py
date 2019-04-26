1e892b30495c45045223466d6d08ce251570695f
vlonly <hushen0721@163.com>
2019-04-26 16:38:41 +0800

    新增3个接口封装

--- api/repositories/traffic.py
+++ api/repositories/traffic.py
@@ -10,3 +10,24 @@ class Traffic(RestClient):
         https://developer.github.com/v3/repos/traffic/#list-referrers
         """
         return self.get("/repos/{}/{}/traffic/popular/referrers".format(owner, repo), **kwargs)
+
+    def list_paths(self, owner, repo, **kwargs):
+        """
+        https://developer.github.com/v3/repos/traffic/#list-paths
+        """
+        return self.get("/repos/{}/{}/traffic/popular/paths".format(owner, repo), **kwargs)
+
+    def list_views(self, owner, repo, **kwargs):
+        """
+        https://developer.github.com/v3/repos/traffic/#views
+        :param kwargs: day,week
+        """
+        return self.get("/repos/{}/{}/traffic/views".format(owner, repo), **kwargs)
+
+    def list_clones(self, owner, repo, **kwargs):
+        """
+        https://developer.github.com/v3/repos/traffic/#clones
+        :param kwargs: day,week
+        """
+        return self.get("/repos/{}/{}/traffic/clones".format(owner, repo), **kwargs)
+
