f5abbefa6bc1cb182f057c47567d5d62d52e7427
vlonly <hushen0721@163.com>
2019-05-07 16:39:27 +0800

    #191,#192,#193,#194,#195

--- github.py
+++ github.py
@@ -1,11 +1,13 @@
 from api.repositories.repos import Repos
 from api.repositories.traffic import Traffic
+from api.repositories.keys import Keys
 from api.issues.issues import Issues
 
 
 class Github():
     def __init__(self, **kwargs):
         self.api_root_url = "https://api.github.com"
-        self.repos = Repos(self.api_root_url, **kwargs)
-        self.issues = Issues(self.api_root_url, **kwargs)
-        self.traffic = Traffic(self.api_root_url, **kwargs)
+        self.repos = Repos(self.api_root_url,**kwargs)
+        self.issues = Issues(self.api_root_url,**kwargs)
+        self.traffic = Traffic(self.api_root_url,**kwargs)
+        self.keys = Keys(self.api_root_url,**kwargs)
