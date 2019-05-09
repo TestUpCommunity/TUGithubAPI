2ff29fa7379f295410ddf061fea27bae80ba6908
xctester <269986229@qq.com>
2019-05-09 10:48:59 +0800

    更新hooks接口封装文件

---
+++ api/repositories/hooks.py
@@ -0,0 +1,23 @@
+from core.rest_client import RestClient
+
+class Hooks(RestClient):
+    def __init__(self, api_root_url, **kwargs):
+        super(Hooks, self).__init__(api_root_url, **kwargs)
+
+    def create_a_hook(self, owner, repo, **kwargs):
+        """
+        https://developer.github.com/v3/repos/hooks/#create-a-hook
+        """
+        return self.post("/repos/{}/{}/hooks".format(owner, repo), **kwargs)
+
+    def edit_a_hook(self, owner,repo, hook_id, **kwargs):
+        """
+        https://developer.github.com/v3/repos/hooks/#edit-a-hook
+        """
+        return self.patch("/repos/{}/{}/hooks/{}".format(owner, repo, hook_id), **kwargs)
+
+    def delete_a_hook(self, owner, repo, hook_id):
+        """
+        https: // developer.github.com / v3 / repos / hooks /  # delete-a-hook
+        """
+        return self.delete_a_hook("/repos/{}/{}/hooks/{}".format(owner, repo, hook_id))
