35a0ce9feb0c660d90625333616c2ee1cc02c89b
vlonly <hushen0721@163.com>
2019-07-10 16:54:04 +0800

    已修改代码问题

--- api/repositories/collaborators.py
+++ api/repositories/collaborators.py
@@ -9,27 +9,27 @@ class Collaborators(RestClient):
         headlers = {'Accept':'application/vnd.github.hellcat-preview+json'}
         return self.get('/repos/{}/{}/collaborators'.format(owner, repo), headlers=headlers, **kwargs)
 
-    def Check_if_a_user_is_a_collaborator(self,owner,repo,username,**kwargs):
+    def check_if_a_user_is_a_collaborator(self,owner,repo,username,**kwargs):
         """
         https://developer.github.com/v3/repos/collaborators/#check-if-a-user-is-a-collaborator
         """
         headlers = {'Accept': 'application/vnd.github.hellcat-preview+json'}
         return self.get('/repos/{}/{}/collaborators/{}'.format(owner,repo,username), headlers=headlers, **kwargs)
 
-    def Review_a_user_permission_level(self,owner,repo,username,**kwargs):
+    def review_a_user_permission_level(self,owner,repo,username,**kwargs):
         """
         https://developer.github.com/v3/repos/collaborators/#review-a-users-permission-level
-        permission: admin, write, read, none
+        :param kwargs:admin，write，read，none。
         """
         return self.get('/repos/{}/{}/collaborators/{}/permission'.format(owner,repo,username), **kwargs)
 
-    def Add_user_as_a_collaborator(self,owner,repo,username,**kwargs):
+    def add_user_as_a_collaborator(self,owner,repo,username,**kwargs):
         """
         https://developer.github.com/v3/repos/collaborators/#add-user-as-a-collaborator
         """
         return self.put('/repos/:owner/{}/collaborators/{}'.format(owner,repo,username), **kwargs)
 
-    def Remove_user_as_a_collaborator(self,owner,repo,username,**kwargs):
+    def remove_user_as_a_collaborator(self,owner,repo,username,**kwargs):
         """
         https://developer.github.com/v3/repos/collaborators/#remove-user-as-a-collaborator
         """
