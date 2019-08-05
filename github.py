b3c48a2de40c4636151ac1d2c318e9b5e996a612
zhangting <89507288@qq.com>
2019-08-05 21:45:57 +0800

    make env change better

--- github.py
+++ github.py
@@ -3,8 +3,8 @@ from api.issues.issues import Issues
 from api.checks.checks import Checks
 
 class Github():
-    def __init__(self, **kwargs):
-        self.api_root_url = "https://api.github.com"
+    def __init__(self, api_root_url, **kwargs):
+        self.api_root_url = api_root_url
         self.repos = Repos(self.api_root_url, **kwargs)
         self.issues = Issues(self.api_root_url, **kwargs)
         self.checks = Checks(self.api_root_url, **kwargs)
