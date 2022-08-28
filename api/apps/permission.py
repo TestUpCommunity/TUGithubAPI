c92bd2534188a36445bea64bcb708c5e6b720ac4
newererer <2915467924@qq.com>
2022-08-28 08:00:33 +0800

    封装API https://docs.github.com/en/rest/users/keys#get-a-public-ssh-key-for-the-authenticated-user

--- api/apps/permission.py
+++ api/apps/permission.py
@@ -19,3 +19,13 @@ class Permission(RestClient):
         """
         headers = {'Accept': 'application/vnd.github+json'}
         return self.post("/user/keys", headers=headers, **kwargs)
+
+    def get_a_ssh_for_current_user(self, key_id, **kwargs):
+        """
+        https://docs.github.com/en/rest/users/keys#get-a-public-ssh-key-for-the-authenticated-user
+        :param key_id: SSH key id
+        :param kwargs:
+        :return:
+        """
+        headers = {'Accept': 'application/vnd.github+json'}
+        return self.get("/user/keys/{}".format(key_id), headers=headers, **kwargs)
